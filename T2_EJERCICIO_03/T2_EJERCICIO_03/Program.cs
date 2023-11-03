fec88c9ea8669519c916a86ae6bca4996f300846
RayoCristian <143460474+RAYO-MONTOYA-CRISTIAN@users.noreply.github.com>
2023-11-02 19:48:11 -0500

    Add files via upload

---
+++ T2_EJERCICIO_03/T2_EJERCICIO_03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2_EJERCICIO_03
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] velocidadesEnMillas = { 20, 40, 60, 80, 100, 120, 140, 160, 180, 200 };
+            Radar radar = new Radar(velocidadesEnMillas);
+
+            radar.MostrarResultados();
+
+            double velocidadMinima = radar.CalcularVelocidadMinima();
+            double velocidadMaxima = radar.CalcularVelocidadMaxima();
+            int velocidadesSuperiores100 = radar.ContarVelocidadesSuperiores100();
+
+            Console.WriteLine("\nVelocidad mínima en kilómetros: " + velocidadMinima.ToString("N2"));
+            Console.WriteLine("Velocidad máxima en kilómetros: " + velocidadMaxima.ToString("N2"));
+            Console.WriteLine("Cantidad de velocidades que superan los 100 km/h: " + velocidadesSuperiores100);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ T2_EJERCICIO_03/T2_EJERCICIO_03/Radar.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2_EJERCICIO_03
+{
+    public class Radar
+    {
+        public int[] velocidadMillas;
+        public double[] frecuencia;
+        public double[] velocidadKilometros;
+        public Radar(int[] millas)
+        {
+            velocidadMillas = millas;
+            frecuencia = new double[millas.Length];
+            velocidadKilometros = new double[millas.Length];
+            CalcularFrecuenciaYVelocidadKm();
+        }
+
+        public void CalcularFrecuenciaYVelocidadKm()
+        {
+            double frecuenciaOriginal = 2500; 
+
+            for (int i = 0; i < velocidadMillas.Length; i++)
+            {
+                velocidadKilometros[i] = velocidadMillas[i] * 1.60934;
+                frecuencia[i] = frecuenciaOriginal * velocidadMillas[i] / 334.8;
+            }
+        }
+
+        public double CalcularVelocidadMinima()
+        {
+            double velocidadMinima = velocidadKilometros[0];
+            foreach (double velocidad in velocidadKilometros)
+            {
+                if (velocidad < velocidadMinima)
+                {
+                    velocidadMinima = velocidad;
+                }
+            }
+            return velocidadMinima;
+        }
+
+        public double CalcularVelocidadMaxima()
+        {
+            double velocidadMaxima = velocidadKilometros[0];
+            foreach (double velocidad in velocidadKilometros)
+            {
+                if (velocidad > velocidadMaxima)
+                {
+                    velocidadMaxima = velocidad;
+                }
+            }
+            return velocidadMaxima;
+        }
+
+        public int ContarVelocidadesSuperiores100()
+        {
+            int velocidadesSuperiores100 = 0;
+            foreach (double velocidad in velocidadKilometros)
+            {
+                if (velocidad > 100)
+                {
+                    velocidadesSuperiores100++;
+                }
+            }
+            return velocidadesSuperiores100;
+        }
+
+        public void MostrarResultados()
+        {
+            Console.Write("\t\tVELOCIDAD DE CARROS \r\n" +
+                "---------------------------------------------------\r\n" +
+                "FRECUENCIA RADAR\t2500 Megaciclos/seg\r\n" +
+                "---------------------------------------------------\r\n" +
+                "VELOCIDAD(mi)\t FRECUENCIA\t      VELOCIDAD(km)\r\n");
+
+            for (int i = 0; i < velocidadMillas.Length; i++)
+            {
+                Console.WriteLine($"   {velocidadMillas[i],-15}{frecuencia[i],-23:N2}{velocidadKilometros[i],-16:N2}");
+            }
+        }
+
+
+    }
+}
